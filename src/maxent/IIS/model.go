2fb23b1daf1dc9773f49c0344c3863bcb6699636
chongliang <chongliang@laiye.com>
2018-12-24 07:57:32 +0800

    优化流程  debuging.

--- src/maxent/IIS/model.go
+++ src/maxent/IIS/model.go
@@ -7,9 +7,7 @@ import (
 	"math/rand"
 	"maxent/dataformat"
 	"os"
-	"strconv"
 	"strings"
-	"sync"
 	"time"
 )
 
@@ -17,20 +15,17 @@ type MaxEntIIS struct {
 	test  []*data.MnistSample
 	train []*data.MnistSample
 
-	XFeatureNum  int
-	w            [][]float64
-	expW         [][]float64
-	probSampleX  map[int]float64
-	probSampleXY map[string]float64
+	xDimension     int
+	featureMap     map[string]*data.FuncFeature
+	featureArray   []*data.FuncFeature
+	featureFuncLen int
+	allPwXy        []float64
 
 	labelYCount int
 	M           float64
-	N           float64
-
-	featureRecord map[int]bool
+	N           int
+	probX       float64
 }
-
-const SplitCount = 3
 
 func (m *MaxEntIIS) LoadData(trainPath, testPath string) {
 	var yCount int
@@ -40,229 +35,123 @@ func (m *MaxEntIIS) LoadData(trainPath, testPath string) {
 		panic("output label not equal between training and test set")
 	}
 
-	m.XFeatureNum = m.train[0].GetDataVectorLen()
-	m.N = 1 / float64(len(m.train))
-	rand.Seed(88888)
-	m.w = make([][]float64, m.labelYCount)
-	for i := 0; i < m.labelYCount; i++ {
-		m.w[i] = make([]float64, m.XFeatureNum)
-		for j := 0; j < m.XFeatureNum; j++ {
-			m.w[i][j] = rand.Float64()
+	m.xDimension = m.train[0].GetDataVectorLen()
+	m.featureMap = make(map[string]*data.FuncFeature)
+	m.N = len(m.train)
+	m.probX = float64(m.N)
+	m.allPwXy = make([]float64, m.N)
+	m.M = 1.0 / float64(m.xDimension)
+	for i := 0; i < m.N; i++ {
+		item := m.train[i]
+		for j := 0; j < m.xDimension; j++ {
+			key := fmt.Sprintf(
+				"%d_%d_%d", item.GetLabel(), j, item.GetDataByIndex(j))
+			if feature, ok := m.featureMap[key]; ok {
+				feature.IncCount()
+			} else {
+				m.featureMap[key] = &data.FuncFeature{
+					LabelIndex: item.GetLabel(),
+					XDIndex:    j,
+					XDValue:    item.GetDataByIndex(j),
+					FeatureKey: key,
+					Count:      1,
+				}
+			}
 		}
 	}
-	m.LoadModel()
 
-	m.expW = make([][]float64, m.labelYCount)
-	for i := 0; i < m.labelYCount; i++ {
-		m.expW[i] = make([]float64, m.XFeatureNum)
+	totalCount := 0
+	for _, item := range m.featureMap {
+		totalCount += item.Count
 	}
-	fmt.Println("load data done")
+	m.featureArray = make([]*data.FuncFeature, len(m.featureMap))
 
-	m.initProbSample()
+	arrayIndex := 0
+	rand.Seed(345345)
+	for _, item := range m.featureMap {
+		item.Prob = float64(item.Count) / float64(totalCount)
+		//item.Weight = rand.Float64() / 10
+		m.featureArray[arrayIndex] = item
+		arrayIndex += 1
+	}
+
+	//m.LoadModel()
+
+	fmt.Println("load data done")
 
 	return
 }
 
-func (m *MaxEntIIS) initProbSample() {
-	m.probSampleX = make(map[int]float64)
-	m.probSampleXY = make(map[string]float64)
-	m.featureRecord = make(map[int]bool)
-	globalCounter := 0
-	m.M = 0.0005
-	for _, X := range m.train {
-		label := X.GetLabel()
-		for fi, xi := range X.GetDataVec() {
-			if xi == 1 {
-				globalCounter += 1
-				m.probSampleX[fi] += 1
-				m.featureRecord[fi] = true
-				xyKey := fmt.Sprintf("%d_%d", fi, label)
-				m.probSampleXY[xyKey] += 1
-			}
+func (m *MaxEntIIS) PwXY(sample *data.MnistSample) float64 {
+	Zw := 0.0
+	tmpSum := make([]float64, m.labelYCount)
+	fiY := sample.GetLabel()
+	for i := 0; i < m.labelYCount; i++ {
+		tmpSum[i] = 0.0
+	}
+	for _, feature := range m.featureArray {
+		if feature.XDValue == sample.GetDataByIndex(feature.XDIndex) {
+			tmpSum[feature.LabelIndex] += feature.Weight
 		}
 	}
-	fmt.Println(len(m.featureRecord))
 
-	for key, count := range m.probSampleX {
-		m.probSampleX[key] = count / float64(globalCounter)
+	for i := 0; i < m.labelYCount; i++ {
+		tmpSum[i] = math.Exp(tmpSum[i])
+		Zw += tmpSum[i]
 	}
-
-	for key, count := range m.probSampleXY {
-		m.probSampleXY[key] = count / float64(globalCounter)
-	}
-
-	fmt.Println("init prob done")
+	return tmpSum[fiY] / Zw
 
 }
 
-func (m *MaxEntIIS) computeExpW(label int, group *sync.WaitGroup) {
-
-	defer group.Done()
-
-	subWg := &sync.WaitGroup{}
-	subWg.Add(SplitCount)
-	if SplitCount == 3 {
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-			for fi := 0; fi < 262; fi++ {
-				if _, ok := m.featureRecord[fi]; ok {
-					m.expW[label][fi] = math.Exp(m.w[label][fi])
-				}
-			}
-		}(subWg)
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-			for fi := 262; fi < 522; fi++ {
-				if _, ok := m.featureRecord[fi]; ok {
-					m.expW[label][fi] = math.Exp(m.w[label][fi])
-				}
-			}
-		}(subWg)
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-			for fi := 522; fi < m.XFeatureNum; fi++ {
-				if _, ok := m.featureRecord[fi]; ok {
-					m.expW[label][fi] = math.Exp(m.w[label][fi])
-				}
-			}
-		}(subWg)
-
-	} else if SplitCount == 4 {
-		for i := 0; i < SplitCount; i++ {
-			go func(start int, wg *sync.WaitGroup) {
-				defer wg.Done()
-				for fi := start * 196; fi < (start+1)*196; fi++ {
-					if _, ok := m.featureRecord[fi]; ok {
-						m.expW[label][fi] = math.Exp(m.w[label][fi])
-					}
-				}
-			}(i, subWg)
-		}
-	}
-	subWg.Wait()
-}
-
-func (m *MaxEntIIS) modelCompute(sample *data.MnistSample) float64 {
-	x := sample.GetDataVec()
-	normalization := 0.0
-	numerator := 0.0
-	for yi := range m.w {
-		tmp := 1.0
-		wi := m.expW[yi]
-		for i, value := range x {
-			if value == 1 {
-				tmp *= wi[i]
-			}
-		}
-		if yi == sample.GetLabel() {
-			numerator = tmp
-		}
-		normalization += tmp
-	}
-	return numerator / normalization
-}
-
-func (m *MaxEntIIS) solveBetaDelta(fi, label int) float64 {
-	xyKey := fmt.Sprintf("%d_%d", fi, label)
-	epobf := m.probSampleXY[xyKey]
-
-	result := 0.0
-	for _, item := range m.train {
-		result += m.N * m.modelCompute(item)
-	}
-
-	return math.Log(epobf/result) * m.M
-}
-
-func (m *MaxEntIIS) trainLabels(label int, group *sync.WaitGroup) {
-	defer group.Done()
-
-	subWg := &sync.WaitGroup{}
-	subWg.Add(SplitCount)
-	if SplitCount == 3 {
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-			for fi := 0; fi < 262; fi++ {
-				if _, ok := m.featureRecord[fi]; ok {
-					m.w[label][fi] += m.solveBetaDelta(fi, label)
-				}
-			}
-		}(subWg)
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-			for fi := 262; fi < 522; fi++ {
-				if _, ok := m.featureRecord[fi]; ok {
-					m.w[label][fi] += m.solveBetaDelta(fi, label)
-				}
-			}
-		}(subWg)
-		go func(wg *sync.WaitGroup) {
-			defer wg.Done()
-			for fi := 522; fi < m.XFeatureNum; fi++ {
-				if _, ok := m.featureRecord[fi]; ok {
-					m.w[label][fi] += m.solveBetaDelta(fi, label)
-				}
-			}
-		}(subWg)
-	} else if SplitCount == 4 {
-		for i := 0; i < 4; i++ {
-			go func(start int, wg *sync.WaitGroup) {
-				defer wg.Done()
-				for fi := start * 196; fi < (start+1)*196; fi++ {
-					if _, ok := m.featureRecord[fi]; ok {
-						m.w[label][fi] += m.solveBetaDelta(fi, label)
-					}
-				}
-			}(i, subWg)
-		}
-	}
-	subWg.Wait()
-}
-
-func (m *MaxEntIIS) StartTraining(iter int) {
+func (m *MaxEntIIS) StartTraining(iter int, coreNum int) {
 	for i := 0; i < iter; i++ {
 		start := time.Now()
 		fmt.Println(" ------ ", start, " ------ iter", i)
-		wg := sync.WaitGroup{}
-		wg.Add(m.labelYCount)
-		for li := 0; li < m.labelYCount; li += 1 {
-			m.computeExpW(li, &wg)
-		}
-		wg.Wait()
+		m.calcAllPwXY()
 
-		wg.Add(m.labelYCount)
-		for li := 0; li < m.labelYCount; li += 1 {
-			go m.trainLabels(li, &wg)
+		for _, feature := range m.featureArray {
+			epobf := feature.Prob
+			result := 0.0
+			for ti, item := range m.train {
+				if item.GetLabel() != feature.LabelIndex ||
+					item.GetDataByIndex(feature.XDIndex) != feature.XDValue {
+					continue
+				}
+
+				result += m.allPwXy[ti]
+			}
+			feature.Weight += math.Log((m.probX*epobf)/result) * m.M
 		}
-		wg.Wait()
+
 		fmt.Println("iter", i, "time cost: ", time.Now().Sub(start))
-
-		m.Test()
+		m.Validation()
 	}
-	m.SaveModel()
 }
 
 func (m *MaxEntIIS) Predict(item *data.MnistSample) bool {
-	x := item.GetDataVec()
-	max := 0.0
-	predictLabel := -1
-	for yi := range m.w {
-		tmp := 0.0
-		wi := m.w[yi]
-		for i, value := range x {
-			if value == 1 {
-				tmp += wi[i]
-			}
-		}
-		tmp = math.Exp(tmp)
-		if tmp > max {
-			max = tmp
-			predictLabel = yi
+	tmpSum := make([]float64, m.labelYCount)
+	for _, feature := range m.featureArray {
+		if feature.XDValue == item.GetDataByIndex(feature.XDIndex) {
+			tmpSum[feature.LabelIndex] += feature.Weight
 		}
 	}
-	return predictLabel == item.GetLabel()
+	if tmpSum[0] > tmpSum[1] {
+		return 0 == item.GetLabel()
+	} else {
+		return 1 == item.GetLabel()
+	}
 }
 
+func (m *MaxEntIIS) Validation() {
+	top1Hit := 0
+	vCount := len(m.train[:10000])
+	for i := 0; i < vCount; i++ {
+		if m.Predict(m.test[i]) {
+			top1Hit += 1
+		}
+	}
+	fmt.Println("test accuracy：", float64(top1Hit)/float64(vCount))
+}
 func (m *MaxEntIIS) Test() {
 	top1Hit := 0
 	testCount := len(m.test)
@@ -278,20 +167,12 @@ func (m *MaxEntIIS) SaveModel() {
 	fileName := fmt.Sprintf("./last_model.dat")
 	if f, err := os.Create(fileName); err == nil {
 		defer f.Close()
-		for li := 0; li < m.labelYCount; li += 1 {
-			for fi := 0; fi < m.XFeatureNum; fi += 1 {
-				if fi == m.XFeatureNum-1 {
-					if _, err := f.WriteString(
-						fmt.Sprintf("%f", m.w[li][fi])); err != nil {
-
-					}
-				} else {
-					if _, err := f.WriteString(
-						fmt.Sprintf("%f,", m.w[li][fi])); err != nil {
-
-					}
-				}
-			}
+		for fi := 0; fi < len(m.featureArray); fi += 1 {
+			feature := m.featureArray[fi]
+			f.WriteString(
+				fmt.Sprintf(
+					"%d,%d,%d,%f",
+					feature.LabelIndex, feature.XDIndex, feature.XDValue, feature.Weight))
 			if _, err := f.WriteString("\n"); err != nil {
 				break
 			}
@@ -309,7 +190,7 @@ func (m *MaxEntIIS) LoadModel() bool {
 			line := scanner.Text()
 			items := strings.Split(line, ",")
 			for fi := 0; fi < len(items); fi++ {
-				m.w[label][fi], _ = strconv.ParseFloat(items[fi], 64)
+
 			}
 			if label < m.labelYCount {
 				label++
@@ -320,4 +201,14 @@ func (m *MaxEntIIS) LoadModel() bool {
 		return true
 	}
 	return false
+}
+
+func (m *MaxEntIIS) TestAllPwXY() {
+	m.calcAllPwXY()
+}
+
+func (m *MaxEntIIS) calcAllPwXY() {
+	for i, item := range m.train {
+		m.allPwXy[i] = m.PwXY(item)
+	}
 }
--- src/maxent/dataformat/dataset.go
+++ src/maxent/dataformat/dataset.go
@@ -5,12 +5,30 @@ type MnistSample struct {
 	label   int
 }
 
+type FuncFeature struct {
+	XDIndex    int
+	XDValue    uint8
+	LabelIndex int
+	FeatureKey string
+	Count      int
+	Weight     float64
+	Prob       float64
+}
+
+func (f *FuncFeature) IncCount() {
+	f.Count += 1
+}
+
 func (s *MnistSample) GetDataVectorLen() int {
 	return len(s.dataVec)
 }
 
 func (s *MnistSample) GetDataVec() []uint8 {
 	return s.dataVec
+}
+
+func (s *MnistSample) GetDataByIndex(index int) uint8 {
+	return s.dataVec[index]
 }
 
 func (s *MnistSample) GetLabel() int {
